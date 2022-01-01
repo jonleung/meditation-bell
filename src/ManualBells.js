cc6c9f274e3bc8f68710fd92c4e280e56113c263
Jonathan Leung <me@jonl.org>
2022-01-01 18:36:30 -0500

    Change wordering

--- src/ManualBells.js
+++ src/ManualBells.js
@@ -4,7 +4,7 @@ const ManualBells = (props) => {
   return (
     <div className="ManualBells">
       <h1 className="ManualBells__title title">Manually Ring Bells</h1>
-      <h2 className="ManualBells__description description">Click the buttons below to ring the bell outside of the automatic schedule.</h2>
+      <h2 className="ManualBells__description description">Click the buttons below to manually ring a bell anytime.</h2>
       <ManualBell type="start" />&nbsp;<ManualBell type="end" />
     </div>
   )
